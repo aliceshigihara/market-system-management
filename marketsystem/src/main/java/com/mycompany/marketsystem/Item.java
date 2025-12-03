8e2ba90b5da262535a50615278bfa295b1d29632
Ssofiahc <sofiaho.c@gmail.com>
2025-12-02 22:11:48 -0300

    Enhance Item class with constructor and getters
    Added constructor and getter methods for Item class.

--- marketsystem/src/main/java/com/mycompany/marketsystem/Item.java
+++ marketsystem/src/main/java/com/mycompany/marketsystem/Item.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.marketsystem;
-
-/**
- *
- * @author ifc
- */
 public class Item {
+    String produto;
+    int quantidade;
+    
+    public Item(String produto, int quantidade){
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public String getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
     
 }
